421b40b34fd8b1618318ccda538d559370b1dac2
Emmanuel Izuchukwu <izuchukwue2@gmail.com>
2022-11-08 08:57:29 +0100

    adjusted codes

--- src/resources/components/bar/VendorContactDetailsNavBar.vue
+++ src/resources/components/bar/VendorContactDetailsNavBar.vue
@@ -5,9 +5,7 @@
         <img src="src/assets/image/PayculiarLogo.png" class="w-full h-full">
       </div>
       <div class="mr-[40px] flex items-center gap-5">
-        <BaseIcon
-        size="21"
-        >
+        <BaseIcon size="21">
           <BellRegular />
         </BaseIcon>
         <div class="flex gap-0.5 items-center">
@@ -20,29 +18,26 @@
           </BaseIcon>
         </div>
 
-        <div class="w-[40px] h-[40px]">
-          <button type="button" class="w-full h-full button-container"></button>
-        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import {BellRegular} from '@vicons/fa'
-import {ChevronDown} from '@vicons/fa'
+import { BellRegular } from '@vicons/fa'
+import { ChevronDown } from '@vicons/fa'
 import BaseIcon from "../icon/BaseIcon.vue";
 </script>
 
 <style scoped>
-.nav-container{
+.nav-container {
   background: #F5F7FB 0% 0% no-repeat padding-box;
   border: 1px solid #E9E9E9;
   opacity: 1;
   z-index: 9999999999999;
 }
 
-.button-container{
+.button-container {
   background: #3ABB1B 0% 0% no-repeat padding-box;
   border-radius: 4px;
   opacity: 1;
--- src/resources/components/upload/PropertyBannerUpload.vue
+++ src/resources/components/upload/PropertyBannerUpload.vue
@@ -1,23 +1,14 @@
 <template>
-  <div
-      class="upload-container"
-  >
+  <div class="upload-container">
 
-<!--    <div class="">-->
-<!--      <h2 class="title">Property Banner</h2>-->
-<!--    </div>-->
-    <n-upload
-        class="h-full w-full h-[450px] relative"
-        multiple
-        directory-dnd
-        action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
-        :max="5"
-    >
-      <n-upload-dragger
-          class="h-[350px] w-[85%] m-auto  absolute left-0 right-0 top-0 bottom-0 "
-      >
-        <div class="w-full h-full flex flex-col place-content-center  justify-center">
-          <div >
+    <!--    <div class="">-->
+    <!--      <h2 class="title">Property Banner</h2>-->
+    <!--    </div>-->
+    <n-upload class="h-full w-full h-[450px] relative" multiple directory-dnd :custom-request="customRequest"
+      action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f" :max="5">
+      <n-upload-dragger class="h-[350px] w-[85%] m-auto  absolute left-0 right-0 top-0 bottom-0 ">
+        <div class="w-full h-full flex flex-col place-content-center justify-center">
+          <div>
             <n-icon size="48" :depth="3">
               <archive-icon />
             </n-icon>
@@ -35,14 +26,53 @@
 <script setup>
 import { ArchiveOutline as ArchiveIcon } from "@vicons/ionicons5";
 
+const customRequest = ({
+  file,
+  data,
+  headers,
+  withCredentials,
+  action,
+  onFinish,
+  onError,
+  onProgress
+}) => {
+  const formData = new FormData()
+  if (data) {
+    Object.keys(data).forEach((key) => {
+      formData.append(
+        key,
+        data[key]
+      )
+    })
+  }
+  formData.append(file.name, file.file)
+  lyla
+    .post(action, {
+      withCredentials,
+      headers: headers,
+      body: formData,
+      onUploadProgress: ({ percent }) => {
+        onProgress({ percent: Math.ceil(percent) })
+      }
+    })
+    .then(({ json }) => {
+      message.success(JSON.stringify(json))
+      onFinish()
+    })
+    .catch((error) => {
+      message.success(error.message)
+      onError()
+    })
+}
 </script>
 
 <style scoped>
-.upload-container{
+.upload-container {
   width: 100%;
   background: #FFFFFF 0% 0% no-repeat padding-box;
 }
-.title{
-  font: normal normal medium  Roboto;
+
+.title {
+  font: normal normal medium Roboto;
 }
 </style>
